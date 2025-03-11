9132a63b7e52d6448f7ed287dec51f72f173fc1e
likedentalclinics <tgstrider@hotmail.com>
2025-03-11 09:34:53 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 
 
 // ประกาศค่า URL ที่ใช้เชื่อมต่อกับ Web App
-const WEB_APP_MEMBER_URL = 'XXXXXXXX';
-const WEB_APP_URL = 'XXXXXXXX';
-const WEB_APP_Leave_URL = 'XXXXXXXX';
-const WEB_APP_OT_URL = 'XXXXXXXX';
+const WEB_APP_MEMBER_URL = 'https://script.google.com/macros/s/AKfycbw48SH2wdgMgxYHU2UWGu9N2wWR2l0Vza6BA7f7H-V9TikZUru2_TRDk4QsQpgmozYAgw/exec';
+const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbwJ7QcquMh-HJrOZTKb7PbhrvHt_JUL1OJaQKXIuYOGV_lgudsJfkQYYMd_Rt6NdB_JhA/exec';
+const WEB_APP_Leave_URL = 'https://script.google.com/macros/s/AKfycbzG_o6J7cL_BgCCjB88ULXAOsJx5TJ29Q2GYOBdNBtZ_2ZI7Rjv6BApCwpO-jtaHzSoVw/exec';
+const WEB_APP_OT_URL = 'https://script.google.com/macros/s/AKfycbxhqXkdnJB30SIDfFXi_fr02rVAOtNPiz3u6a0hQJPcfeLSL4WsdsTFjPz6W7frJnkl/exec';
 
 //LIFF_ID
 const LIFF_ID = 'XXXXXXXX'; // LIFF ID Checkin
