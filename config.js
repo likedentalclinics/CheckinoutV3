674d2008895dbf7062e75ad06f9641a6a76437e1
likedentalclinics <tgstrider@hotmail.com>
2025-03-11 09:29:52 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,14 @@
+
+
+// ประกาศค่า URL ที่ใช้เชื่อมต่อกับ Web App
+const WEB_APP_MEMBER_URL = 'XXXXXXXX';
+const WEB_APP_URL = 'XXXXXXXX';
+const WEB_APP_Leave_URL = 'XXXXXXXX';
+const WEB_APP_OT_URL = 'XXXXXXXX';
+
+//LIFF_ID
+const LIFF_ID = 'XXXXXXXX'; // LIFF ID Checkin
+const LIFF_ID1 = 'XXXXXXXX'; // LIFF ID history
+const LIFF_ID2 = 'XXXXXXXX'; // LIFF ID leave
+const LIFF_ID3 = 'XXXXXXXX'; // LIFF ID ot
+const LIFF_ID4 = 'XXXXXXXX'; // LIFF ID register
