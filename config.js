0eda750bd682a118cec9c749be9faacf9c245b28
likedentalclinics <tgstrider@hotmail.com>
2025-05-05 11:37:07 +0700

    Update config.js

--- config.js
+++ config.js
@@ -6,6 +6,7 @@ const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbwJ7QcquMh-HJrOZTKb
 const WEB_APP_Leave_URL = 'https://script.google.com/macros/s/AKfycbzG_o6J7cL_BgCCjB88ULXAOsJx5TJ29Q2GYOBdNBtZ_2ZI7Rjv6BApCwpO-jtaHzSoVw/exec';
 const WEB_APP_OT_URL = 'https://script.google.com/macros/s/AKfycbxhqXkdnJB30SIDfFXi_fr02rVAOtNPiz3u6a0hQJPcfeLSL4WsdsTFjPz6W7frJnkl/exec';
 const CONDUCT_URL = 'https://script.google.com/macros/s/AKfycbxJz3DSvUpX5O7erZHVS95vuByYGLQGSvVdgBQiho7Xw0fItJ6wErovmwSfdFESZpc1Tg/exec';
+const url = 'https://script.google.com/macros/s/AKfycbyi1SPjIJY0x-AChbRtwLv8A2QR532uqjwluDr3Ym0euh7mqiUcXeS3t2BWQY1SSJfrYQ/exec';
 
 //LIFF_ID
 const LIFF_ID = '2007047383-gVW8J9pn'; // LIFF ID Checkin
